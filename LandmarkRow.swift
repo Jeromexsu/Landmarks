02813f1b2ccf607136eb2ee798998b9222641547
Su Chuan <jeromesu@outlook.com>
2024-05-03 16:11:28 +0800

    make landmark list(identifiable, List view)

---
+++ LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by JeremySu on 3/5/2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by JeremySu on 3/5/2024.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
