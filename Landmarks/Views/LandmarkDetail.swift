1e74ac9387c58b047e4846760119facff7e2002b
Su Chuan <jeromesu@outlook.com>
2024-05-03 16:30:43 +0800

    scroll view

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -11,32 +11,36 @@ struct LandmarkDetail: View {
     var landmark: Landmark
     
     var body: some View {
-        MapView(location: landmark.locationCoordinate)
-            .frame(height: 300)
-        
-        CircleImage(image: landmark.image)
-            .offset(y: -130)
-            .padding(.bottom, -130)
-        
-        VStack(alignment: .leading) {
-            Text(landmark.name)
-                .font(.title)
-            HStack {
-                Text(landmark.park)
-                Spacer()
-                Text(landmark.state)
+        ScrollView {
+            MapView(location: landmark.locationCoordinate)
+                .frame(height: 300)
+            
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
             }
-            .font(.subheadline)
-            .foregroundStyle(.secondary)
-            
-            Divider()
-            
-            Text("About \(landmark.name)")
-                .font(.title2)
-            Text(landmark.description)
+            .padding()
         }
-        .padding()
-        Spacer()
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
