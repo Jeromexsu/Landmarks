f6ebdb834af9067365d88e5ae717a5ae179db069
Su Chuan <jeromesu@outlook.com>
2024-05-03 15:59:51 +0800

    load data function

---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by JeremySu on 3/5/2024.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T:Codable> (_ filename: String) -> T {
+    guard let fileURL = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    let data: Data
+    do {
+        data = try Data(contentsOf: fileURL)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+
+    }
+}
