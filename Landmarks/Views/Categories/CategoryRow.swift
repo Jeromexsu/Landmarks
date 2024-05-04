99acf58fe06e43d05162dba0367531f6eeefbc67
Su Chuan <jeromesu@outlook.com>
2024-05-04 12:21:47 +0800

    category home

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by JeremySu on 4/5/2024.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
